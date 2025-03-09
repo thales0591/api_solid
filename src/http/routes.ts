e372f2e031e51740f6fe8fb6ad95421d181500f6
thales0591 <thales0591@gmail.com>
2025-03-09 20:15:29 -0300

    feat: implement authenticate controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { app } from '@/app'
 import { register } from './controllers/register'
+import { authenticate } from './controllers/authenticate'
 
 export async function appRoutes() {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
