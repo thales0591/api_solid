dacf774d77f9d2178903591364023a9acba4240e
thales0591 <thales0591@gmail.com>
2025-03-12 15:52:51 -0300

    feat: integrate JWT for user authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { app } from '@/app'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes() {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
