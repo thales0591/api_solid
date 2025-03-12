622492f5267933628d44a3d6416561ccdd8f3227
thales0591 <thales0591@gmail.com>
2025-03-12 16:52:42 -0300

    feat: improve user profile controller and create verifyJWT middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { app } from '@/app'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes() {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
