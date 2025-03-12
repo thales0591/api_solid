a9402161df71390acf556fbb9d8dd3e4bf884f40
thales0591 <thales0591@gmail.com>
2025-03-11 22:26:43 -0300

    feat: implement user metrics use case and it unit test

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,31 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { GetUserMetricsUseCase } from './get-user-metrics'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: GetUserMetricsUseCase
+
+describe('Get user metrics Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsUseCase(checkInsRepository)
+  })
+
+  it('should be able to get check-ins count from metrics', async () => {
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-02',
+    })
+
+    const { checkInsCount } = await sut.execute({
+      userId: 'user-01',
+    })
+
+    expect(checkInsCount).toEqual(2)
+  })
+})
