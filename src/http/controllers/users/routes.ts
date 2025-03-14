563afb1301c63512bf7a895e3b1d0ede3c31749c
thales0591 <thales0591@gmail.com>
2025-03-13 23:04:45 -0300

    feat: implement gym creation controller

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,13 @@
+import { register } from './register'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { verifyJWT } from '../../middlewares/verify-jwt'
+import { FastifyInstance } from 'fastify'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
+}
