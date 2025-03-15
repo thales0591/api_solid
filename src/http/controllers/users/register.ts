39faf7fc57addf256bf18ed7efaf765bca72cab9
thales0591 <thales0591@gmail.com>
2025-03-15 12:46:41 -0300

    feat: implement RBAC validation

--- src/http/controllers/users/register.ts
+++ src/http/controllers/users/register.ts
@@ -6,7 +6,7 @@ import { z } from 'zod'
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
-    email: z.string(),
+    email: z.string().email(),
     password: z.string().min(6),
   })
 
